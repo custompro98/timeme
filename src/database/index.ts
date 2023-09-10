64b40220ff6576d69c25a81df3ac0fe24076cdd0
custompro98 <mitchjoa@gmail.com>
2023-09-10 19:13:15 -0400

    sqlite

---
+++ src/database/index.ts
@@ -0,0 +1,15 @@
+import Database from "bun:sqlite";
+
+export const db = new Database("timeme.sqlite3", { create: true });
+
+const handleClose = (code: string) => {
+  console.log("closing...");
+  db.close();
+
+  console.log(`exiting (${code})...`);
+  process.exit(parseInt(code, 10));
+};
+
+process.on("SIGINT", handleClose);
+process.on("SIGTERM", handleClose);
+process.on("SIGKILL", handleClose);
